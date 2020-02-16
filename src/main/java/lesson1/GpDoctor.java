36c5e85b72197d169655e064b355a24b11e2b4d6
asbyvshev <barseto4ka@live.ru>
2020-02-16 15:46:01 +0300

    Добавлена инфа о кабинете.

--- src/main/java/lesson1/GpDoctor.java
+++ src/main/java/lesson1/GpDoctor.java
@@ -1,8 +1,14 @@
 package lesson1;
 
 public class GpDoctor implements Doctor {
+    private int office = 321;
+
+    public int getOffice() {
+        return office;
+    }
+
     @Override
     public void processing() {
-        System.out.println("Вы на приеме у Терапевта!");
+        System.out.format("Вас примет Терапевт в кабенете № %d!",getOffice());
     }
 }
--- src/main/java/lesson1/Surgeon.java
+++ src/main/java/lesson1/Surgeon.java
@@ -1,8 +1,14 @@
 package lesson1;
 
 public class Surgeon implements Doctor {
+    private int office = 101;
+
+    public int getOffice() {
+        return office;
+    }
+
     @Override
     public void processing() {
-        System.out.println("Вы на приеме у Хирурга!");
+        System.out.format("Вас примет Хирург в кабенете № %d!",getOffice());
     }
 }
